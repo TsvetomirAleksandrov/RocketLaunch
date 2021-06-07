6281fa04c83a830554ec11c242cef4e6e4969cb0
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-07 20:24:33 +0300

    Create Footer Component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer bg-dark text-center text-light text-lg-start">
+                <div className="text-center p-3">
+                    © 2021 Copyright: Tsvetomir Aleksandrov
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
