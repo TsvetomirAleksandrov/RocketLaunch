4ad8cf3b759159a76e7407edbc1d0162cb40e821
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-06-21 12:19:14 +0300

    Update App Component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -5,19 +5,28 @@ import Footer from '../Footer/Footer';
 import DetailsPage from '../DetailsPage/DetailsPage';
 import LandingPage from '../LandingPage/LandingPage';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useReducer } from 'react';
+import reducer from '../reducer';
+import { PageNumberContext } from '../context/context';
+
 
 function App() {
+  const initialState = 1;
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
     <Router>
       <div className="App">
-        <Header />
+        <PageNumberContext.Provider value={{ counterCount: count, counterDispatch: dispatch }}>
+          <Header />
 
-        <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/details/:launchId' component={DetailsPage} />
-        </Switch>
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/details/:launchId' component={DetailsPage} />
+          </Switch>
 
-        <Footer />
+          <Footer />
+        </PageNumberContext.Provider>
       </div>
     </Router>
   );
